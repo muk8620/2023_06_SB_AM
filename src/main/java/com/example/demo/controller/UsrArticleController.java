8fecd9a628d845b87dc268e23d0b7365508d5919
muk7777 <muk8620@gmail.com>
2023-06-16 21:49:31 +0900

    delete, modify, detail(service,dao 분리는 안함)

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -14,20 +14,22 @@ public class UsrArticleController {
 	private int lastArticleId;
 	private List<Article> articles;
 	
+	
 	UsrArticleController() {
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
 		makeTestData();
 	}
 	
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			
-			String title = "제목" + i;
-			String body = "내용" + i;
-			
-			writeArticle(title, body);
+//	서비스메서드
+	private Article getArticleById(int id) {
+		
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
 		}
+		return null;
 	}
 	
 	private Article writeArticle(String title, String body) {
@@ -40,7 +42,30 @@ public class UsrArticleController {
 		
 		return article;
 	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticleById(id);
+		articles.remove(article);
+	}
+	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticleById(id);
+		article.setTitle(title);
+		article.setBody(body);
+	}
+	
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			writeArticle(title, body);
+		}
+	}
+	
 
+//	액션메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -54,6 +79,49 @@ public class UsrArticleController {
 	public List<Article> getArticles() {
 		
 		return articles;
+	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		
+		Article foundArticle = getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다.";
+		}
+		
+		deleteArticle(id);
+		
+		return id + "번 게시글을 삭제했습니다.";
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		
+		Article foundArticle = getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다.";
+		}
+		
+		modifyArticle(id, title, body);
+		
+		return id + "번 게시글을 수정했습니다.";
+	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticle(int id) {
+		
+		Article foundArticle = getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다.";
+		}
+		
+		return foundArticle;
 	}
 }
 
--- src/main/java/com/example/demo/vo/Article.java
+++ src/main/java/com/example/demo/vo/Article.java
@@ -2,9 +2,11 @@ package com.example.demo.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Article {
 	private int id;
 	private String title;
