4ecc0e6b98d1d36cc388b36a302902559ec24f66
muk7777 <muk8620@gmail.com>
2023-06-19 21:52:27 +0900

    xml방식으로 쿼리 넘기기(어노테이션 방식은 다음 커밋에)

--- src/main/java/com/example/demo/dao/ArticleDao.java
+++ src/main/java/com/example/demo/dao/ArticleDao.java
@@ -14,25 +14,11 @@ public interface ArticleDao {
 
 	public Article writeArticle(String title, String body);
 
-	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticleById(int id);
 	
-	@Update("""
-			UPDATE article 
-				set updateDate = now()
-					, title = #{title}
-					, `body` = #{body}
-				where id = #{id}
-			""")
 	public void modifyArticle(int id, String title, String body);
 
-	@Delete("DELETE from article where id = #{id}")
 	public void deleteArticle(int id); 
 	
-	@Select("""
-			SELECT * 
-				FROM article 
-				ORDER BY id DESC
-			""")
 	public List<Article> getArticles(); 
 }
