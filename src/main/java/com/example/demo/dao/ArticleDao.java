f320710d08bcbb93c4d051627836a3dc1d994f78
muk7777 <muk8620@gmail.com>
2023-06-20 16:33:19 +0900

    Mapper방식으로 쿼리 넘기기
    script 태그를 위아래로 붙이면 동적SQL 사용 가능

--- src/main/java/com/example/demo/dao/ArticleDao.java
+++ src/main/java/com/example/demo/dao/ArticleDao.java
@@ -11,14 +11,43 @@ import com.example.demo.vo.Article;
 
 @Mapper
 public interface ArticleDao {
-
+	
 	public Article writeArticle(String title, String body);
-
+	
+	@Select("""
+			select * 
+				from article
+				where id = #{id}
+			""")
 	public Article getArticleById(int id);
 	
+	@Update("""
+			<script>
+				update article
+					<set>
+						updateDate = now()
+						<if test="title != null and title != ''">
+							, title = #{title}
+						</if>	
+						<if test="body != null and body != ''">
+							, `body` = #{body}
+						</if>	
+					</set>
+					where id = #{id}
+			</script>
+			""")
 	public void modifyArticle(int id, String title, String body);
 
+	@Delete("""
+			delete from article
+				where id = #{id}
+			""")
 	public void deleteArticle(int id); 
 	
+	@Select("""
+			select *
+				from article
+				order by id DESC
+			""")
 	public List<Article> getArticles(); 
 }
