1531e201ffc82e5b4ed6685479687ba2efa840cb
muk7777 <muk8620@gmail.com>
2023-07-14 19:51:57 +0900

    댓글 수정 취소 및 수정 중 다른 댓글 수정 시 기존 수정 취소 cleanCode

--- src/main/java/com/example/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/example/demo/config/MyWebMvcConfigurer.java
@@ -45,6 +45,9 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 		ir.addPathPatterns("/usr/reactionPoint/doDeleteReactionPoint");
 		ir.addPathPatterns("/usr/reactionPoint/doDeleteReactionPoint");
 		ir.addPathPatterns("/usr/reply/doWrite");
+		ir.addPathPatterns("/usr/reply/doModify");
+		ir.addPathPatterns("/usr/reply/doDelete");
+		ir.addPathPatterns("/usr/reply/getReplyContent");
 	}
 
 }
